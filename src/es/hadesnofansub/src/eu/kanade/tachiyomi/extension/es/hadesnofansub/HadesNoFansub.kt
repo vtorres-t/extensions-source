e07b236a2e2a2d4145d8fa486dccf838ae15e3ae
Vicente <vicente@per>
2026-06-02 16:32:56 +0200

    Fix date Hades no Fansub

--- src/es/hadesnofansub/src/eu/kanade/tachiyomi/extension/es/hadesnofansub/HadesNoFansub.kt
+++ src/es/hadesnofansub/src/eu/kanade/tachiyomi/extension/es/hadesnofansub/HadesNoFansub.kt
@@ -1,12 +1,15 @@
 package eu.kanade.tachiyomi.extension.es.hadesnofansub
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class HadesNoFansub :
     Madara(
         "Hades no Fansub",
         "https://lectorhades.latamtoon.com",
         "es",
+        dateFormat = SimpleDateFormat("MM/dd/yyyy", Locale("es")),
     ) {
     override val useNewChapterEndpoint = true
 
@@ -14,7 +17,7 @@ class HadesNoFansub :
 
     override val useLoadMoreRequest = LoadMoreStrategy.Never
 
-    override val mangaDetailsSelectorStatus = "div.post-content_item:has(h5:contains(Status)) div.summary-content"
+    override val mangaDetailsSelectorStatus = "div.summary_content > div.post-content div.post-content_item:has(div.summary-heading:contains(Status)) div.summary-content"
 
     override val mangaDetailsSelectorTag = "div.tags-content a.notUsed" // Site uses this for the scanlator
 }

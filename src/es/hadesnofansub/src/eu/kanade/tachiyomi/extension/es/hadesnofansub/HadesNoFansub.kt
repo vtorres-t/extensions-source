db7b5c6fe2cdb3b1c0fe7c214df1c548edc8dbdc
Vicente <vicente@per>
2026-06-02 13:34:18 +0200

    Fix date Hades no Fansub

--- src/es/hadesnofansub/src/eu/kanade/tachiyomi/extension/es/hadesnofansub/HadesNoFansub.kt
+++ src/es/hadesnofansub/src/eu/kanade/tachiyomi/extension/es/hadesnofansub/HadesNoFansub.kt
@@ -1,8 +1,6 @@
 package eu.kanade.tachiyomi.extension.es.hadesnofansub
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 class HadesNoFansub :
     Madara(

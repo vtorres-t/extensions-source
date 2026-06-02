21af967a6c03b9d3f56f7818eb8f2f6d3ed849d8
Vicente <vicente@per>
2026-06-02 16:46:43 +0200

    Fix Hades no Fansub

--- src/es/hadesnofansub/src/eu/kanade/tachiyomi/extension/es/hadesnofansub/HadesNoFansub.kt
+++ src/es/hadesnofansub/src/eu/kanade/tachiyomi/extension/es/hadesnofansub/HadesNoFansub.kt
@@ -9,7 +9,7 @@ class HadesNoFansub :
         "Hades no Fansub",
         "https://lectorhades.latamtoon.com",
         "es",
-        dateFormat = SimpleDateFormat("MM/dd/yyyy", Locale("es")),
+        dateFormat = SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
     ) {
     override val useNewChapterEndpoint = true
 
@@ -17,7 +17,9 @@ class HadesNoFansub :
 
     override val useLoadMoreRequest = LoadMoreStrategy.Never
 
-    override val mangaDetailsSelectorStatus = "div.summary_content > div.post-content div.post-content_item:has(div.summary-heading:contains(Status)) div.summary-content"
+    override val mangaDetailsSelectorStatus = "div.post-content_item:has(h5:contains(Status)) div.summary-content"
 
     override val mangaDetailsSelectorTag = "div.tags-content a.notUsed" // Site uses this for the scanlator
+
+    override fun chapterDateSelector() = "span.chapter-release-date span.timediff i"
 }

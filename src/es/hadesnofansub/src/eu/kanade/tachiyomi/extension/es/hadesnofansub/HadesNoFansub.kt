7ab7a6db2ac1e9530a9fa14cc5c714c75c830c8b
Vicente <vicente@per>
2026-09-17 14:04:45 +0200

    Sync

--- src/es/hadesnofansub/src/eu/kanade/tachiyomi/extension/es/hadesnofansub/HadesNoFansub.kt
+++ src/es/hadesnofansub/src/eu/kanade/tachiyomi/extension/es/hadesnofansub/HadesNoFansub.kt
@@ -1,15 +1,13 @@
 package eu.kanade.tachiyomi.extension.es.hadesnofansub
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
-import eu.kanade.tachiyomi.source.model.SChapter
 import keiyoushi.annotation.Source
-import org.jsoup.nodes.Element
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 @Source
 abstract class HadesNoFansub : Madara() {
-    override val chapterDateFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy", Locale("es"))
+    override val chapterDateFormat = DateTimeFormatter.ofPattern("MMMM d, yyyy", Locale("es"))
     override val chapterMode = ChapterMode.MangaAjax
 
     override val mangaSubString = "tmo"
@@ -18,18 +16,4 @@ abstract class HadesNoFansub : Madara() {
 
     override val mangaDetailsSelectorTag = "div.tags-content a.notUsed" // Site uses this for the scanlator
 
-    override fun chapterFromElement(element: Element, mangaPath: String): SChapter? {
-        val chapter = super.chapterFromElement(element, mangaPath) ?: return null
-
-        val dateElement = element.selectFirst("span.chapter-release-date span.timediff i")
-        if (dateElement != null) {
-            val dateText = dateElement.text().trim()
-            chapter.date_upload = parseChapterDate(dateText)
-        } else {
-            val backupText = element.selectFirst("span.chapter-release-date")?.text()
-            chapter.date_upload = parseChapterDate(backupText)
-        }
-
-        return chapter
-    }
 }

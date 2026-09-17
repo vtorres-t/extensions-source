483fdfc16e1cd4f65c0bdb800fae07dea787906c
Vicente <vicente@per>
2026-09-17 14:08:55 +0200

    Sync

--- src/es/hadesnofansub/src/eu/kanade/tachiyomi/extension/es/hadesnofansub/HadesNoFansub.kt
+++ src/es/hadesnofansub/src/eu/kanade/tachiyomi/extension/es/hadesnofansub/HadesNoFansub.kt
@@ -7,7 +7,7 @@ import java.util.Locale
 
 @Source
 abstract class HadesNoFansub : Madara() {
-    override val chapterDateFormat = DateTimeFormatter.ofPattern("MMMM d, yyyy", Locale("es"))
+    override val chapterDateFormat = DateTimeFormatter.ofPattern("MMMM dd, yyyy", Locale("es"))
     override val chapterMode = ChapterMode.MangaAjax
 
     override val mangaSubString = "tmo"
@@ -15,5 +15,4 @@ abstract class HadesNoFansub : Madara() {
     override val mangaDetailsSelectorStatus = "div.summary_content > div.post-content div.post-content_item:has(div.summary-heading:contains(Status)) div.summary-content"
 
     override val mangaDetailsSelectorTag = "div.tags-content a.notUsed" // Site uses this for the scanlator
-
 }
